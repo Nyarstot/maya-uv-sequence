28c71c5ed55f2b0eeb12b7e4ab95d8b587e0ee96
Nikita Kozlov <Nyarstot@protonmail.com>
2022-02-01 05:34:00 +0300

    fix: imports

--- uv-sequence.py
+++ uv-sequence.py
@@ -1,7 +1,11 @@
-selection = cmds.ls(sl=True)
-firstObject = selection[0]
+import maya.cmds as cmds
 
-for i in range(1, len(selection)):
-    # uvs = 2 (transfer uvs 'all')
-    # sampleSpace = 2 (component)
-    cmds.transferAttributes(selection[0], selection[i], uvs = 2, sampleSpace = 2)
+selection = cmds.ls(sl=True)
+
+if len(selection) > 1:
+    for i in range(1, len(selection)):
+        # uvs = 2 (transfer uvs 'all')
+        # sampleSpace = 2 (component)
+        cmds.transferAttributes(selection[0], selection[i], uvs = 2, sampleSpace = 2)
+else:
+    cmds.error("The number of selected objects must be greater than one")
