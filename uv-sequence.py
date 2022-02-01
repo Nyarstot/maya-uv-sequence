16d8da9ccc32eff8062da9a62790b4d0fae2454a
Nikita Kozlov <Nyarstot@protonmail.com>
2022-02-01 06:25:26 +0300

    add icon; fix: sampleSpace parameter

--- uv-sequence.py
+++ uv-sequence.py
@@ -5,7 +5,7 @@ selection = cmds.ls(sl=True)
 if len(selection) > 1:
     for i in range(1, len(selection)):
         # uvs = 2 (transfer uvs 'all')
-        # sampleSpace = 2 (component)
-        cmds.transferAttributes(selection[0], selection[i], uvs = 2, sampleSpace = 2)
+        # sampleSpace = 4 (component)
+        cmds.transferAttributes(selection[0], selection[i], uvs = 2, sampleSpace = 4)
 else:
     cmds.error("The number of selected objects must be greater than one")
