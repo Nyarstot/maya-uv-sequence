4d1eb3e33e87d764045f3edbea1557ba3511d1d6
Nikita Kozlov <Nyarstot@protonmail.com>
2022-04-19 02:06:05 +0300

    Added copyright and update gitignore

--- uv-sequence.py
+++ uv-sequence.py
@@ -1,11 +1,12 @@
+# Author: Nikita Kozlov
+# Link: github.com/nyarstot
+
 import maya.cmds as cmds
 
 selection = cmds.ls(sl=True)
 
 if len(selection) > 1:
     for i in range(1, len(selection)):
-        # uvs = 2 (transfer uvs 'all')
-        # sampleSpace = 4 (component)
         cmds.transferAttributes(selection[0], selection[i], uvs = 2, sampleSpace = 4)
 else:
     cmds.error("The number of selected objects must be greater than one")
